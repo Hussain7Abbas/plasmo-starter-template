aab413eb82a6a4f7d4edfc349329011fbd0f200f
hussain7abbas <hussain.abduljaleel1905@coeng.uobaghdad.edu.iq>
2025-03-11 17:08:11 +0300

    feat(mantine): integrate

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,26 +1,15 @@
-import React, { useState } from "react"
+import React from 'react';
+import '@mantine/core/styles.css';
+import '~/styles/global.css';
+import { theme } from '~/styles/theme';
+import { Button, MantineProvider } from '@mantine/core';
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank" rel="noreferrer">
-          Plasmosssx
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank" rel="noreferrer">
-        View Docs
-      </a>
-    </div>
-  )
+    <MantineProvider theme={theme}>
+      <Button>Hello</Button>
+    </MantineProvider>
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
